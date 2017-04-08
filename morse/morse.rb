ec0a84e0da9f44d84f2db1dc9d9f3645848b9d4c
Daniel Ferraz <d.ferrazm@gmail.com>
2017-04-08 22:05:01 +0200

    Refactor morse encoder.

--- morse/morse.rb
+++ morse/morse.rb
@@ -4,17 +4,38 @@ class Morse
   DEFINITIONS = YAML.load_file('definitions.yml').freeze
   WORD_SEPARATOR = '/'.freeze
   CHAR_SEPARATOR = '|'.freeze
-  WHITESPACE = ' '
 
   def encode(text)
-    words = text.split(WHITESPACE)
+    InputText.new(text).encoded
+  end
 
-    encoded_words = words.map do |word|
-      encoded_chars = word.chars.map do |char|
-        DEFINITIONS[char]
-      end.join(CHAR_SEPARATOR)
+  class InputText
+    def initialize(text)
+      @text = text
     end
 
-    encoded_words.join(WORD_SEPARATOR)
+    def encoded
+      encoded_words.join(WORD_SEPARATOR)
+    end
+
+    private
+
+    def words
+      @text.split(' ')
+    end
+
+    def encoded_words
+      words.map { |word| encoded_word(word) }
+    end
+
+    def encoded_word(word)
+      encoded_chars(word.chars).join(CHAR_SEPARATOR)
+    end
+
+    def encoded_chars(chars)
+      chars.map do |char|
+        DEFINITIONS[char]
+      end
+    end
   end
 end
