a8bbd95c77d4689ac01725b00bd883b6feff2543
Daniel Ferraz <d.ferrazm@gmail.com>
2017-04-08 21:49:50 +0200

    Add the first draft of the morse encoder implementation.

--- morse/morse.rb
+++ morse/morse.rb
@@ -2,8 +2,19 @@ require 'yaml'
 
 class Morse
   DEFINITIONS = YAML.load_file('definitions.yml').freeze
+  WORD_SEPARATOR = '/'.freeze
+  CHAR_SEPARATOR = '|'.freeze
+  WHITESPACE = ' '
 
-  def encode(char)
-    DEFINITIONS[char]
+  def encode(text)
+    words = text.split(WHITESPACE)
+
+    encoded_words = words.map do |word|
+      encoded_chars = word.chars.map do |char|
+        DEFINITIONS[char]
+      end.join(CHAR_SEPARATOR)
+    end
+
+    encoded_words.join(WORD_SEPARATOR)
   end
 end
--- morse/morse_test.rb
+++ morse/morse_test.rb
@@ -11,4 +11,32 @@ class TestMorse < Minitest::Test
       assert_equal code, @morse.encode(char)
     end
   end
+
+  def test_encoding_of_hello_world
+    assert_equal(
+      '....|.|.-..|.-..|---/.--|---|.-.|.-..|-..',
+      @morse.encode('HELLO WORLD')
+    )
+  end
+
+  def test_encoding_of_text_containing_all_letters
+    assert_equal(
+      '-|....|./--.-|..-|..|-.-.|-.-/-...|.-.|---|.--|-./..-.|---|-..-/.---|..-|--|.--.|.../---|...-|.|.-./-|....|./.-..|.-|--..|-.--/-..|---|--.',
+      @morse.encode('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')
+    )
+  end
+
+  def test_encoding_of_text_containing_all_numbers
+    assert_equal(
+      '----.|.----|---..|..---|--...|...--|-....|....-|.....',
+      @morse.encode('918273645')
+    )
+  end
+
+  def test_encoding_of_text_containing_all_characters
+    assert_equal(
+      '.----/-|....|./..---/--.-|..-|..|-.-.|-.-/...--/-...|.-.|---|.--|-./....-/..-.|---|-..-/.....|--..--/.---|..-|--|.--.|.../-..../---|...-|.|.-./--.../-|....|./---../.-..|.-|--..|-.--/----./-..|---|--./.----|-----|.-.-.-',
+      @morse.encode('1 THE 2 QUICK 3 BROWN 4 FOX 5, JUMPS 6 OVER 7 THE 8 LAZY 9 DOG 10.')
+    )
+  end
 end
