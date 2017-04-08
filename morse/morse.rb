04c1895482267a81868c47b74db7c91bcc2a93ae
Daniel Ferraz <d.ferrazm@gmail.com>
2017-04-09 00:45:33 +0200

    Implement obfuscator.

--- morse/morse.rb
+++ morse/morse.rb
@@ -17,6 +17,47 @@ module Morse
     end
   end
 
+  class ObfuscatedDefinitions
+    def initialize(definitions)
+      @definitions = definitions
+    end
+
+    def [](char)
+      code = @definitions[char]
+      obfuscated(code)
+    end
+
+    private
+
+    def obfuscated(code)
+      obfuscated_sequences(code).join
+    end
+
+    def obfuscated_sequences(code)
+      char_sequences(code).map do |sequence, type|
+        OBFUSCATOR[type].call(sequence)
+      end
+    end
+
+    # This regex will scan the morse code to find the repeated characters.
+    # It returns an array of 2-tuples, the first element being the sequence
+    # and second element being the repeated character. Ex:
+    # Input: '...--..-'
+    # Output: [['...','.'], ['--','-'], ['..','.'],['-','-']]
+    def char_sequences(code)
+      code.scan(/((.)\2*)/)
+    end
+
+    ALPHABET = ('A'..'Z').to_a.freeze
+    DOT = '.'.freeze
+    DASH = '-'.freeze
+
+    OBFUSCATOR = {
+      DOT => ->(dots) { dots.length },
+      DASH => ->(dashes) { ALPHABET[dashes.length - 1] }
+    }
+  end
+
   DEFINITIONS = Definitions.new(YAML.load_file('definitions.yml')).freeze
   WORD_SEPARATOR = '/'.freeze
   CHAR_SEPARATOR = '|'.freeze
--- morse/morse_test.rb
+++ morse/morse_test.rb
@@ -1,6 +1,12 @@
 require 'minitest/autorun'
 require_relative 'morse'
 
+# NOTE: The following tests are excercising only the `Morse::Encoder` because that's
+# the intended interface that clients of this class will make use. As all the other
+# classes are being used under the hood, they are being tested as well. But of course,
+# to achieve a more granular unit testing, these classes could also be easily
+# unit tested, should that be required.
+
 class TestMorseEncoder < Minitest::Test
   def test_that_the_definitions_are_correctly_encoded
     definitions = YAML.load_file('definitions.yml')
@@ -63,10 +69,55 @@ class TestMorseEncoder < Minitest::Test
     )
   end
 
+  def test_raise_an_error_when_input_contains_unmapped_characters
+    assert_raises Morse::InvalidChar do morse.encode('HELP!') end
+  end
+
+  private
+
+  def morse
+    @morse ||= Morse::Encoder.new
+  end
+end
+
+class TestObfuscatedMorseEncoder < Minitest::Test
+  def test_obfuscation
+    assert_equal(
+      '2/1A|B/2|A1/A|1A1|C|2A|A3|1A2|1',
+      morse.encode('I AM IN TROUBLE')
+    )
+  end
+
+  def test_encoding_of_text_containing_all_characters
+    assert_equal(
+      '1D/A|4|1/2C/B1A|2A|2|A1A1|A1A/3B/A3|1A1|C|1B|A1/4A/2A1|C|A2A/5|B2B/1C|2A|B|1B1|3/A4/C|3A|1|1A1/B3/A|4|1/C2/1A2|1A|B2|A1B/D1/A2|C|B1/1D|E|1A1A1A',
+      morse.encode('1 THE 2 QUICK 3 BROWN 4 FOX 5, JUMPS 6 OVER 7 THE 8 LAZY 9 DOG 10.')
+    )
+  end
+
   def test_encoding_of_text_with_trailing_spaces
     assert_equal(
-      '../.-|--/..|-./-|.-.|---|..-|-...|.-..|.',
+      '2/1A|B/2|A1/A|1A1|C|2A|A3|1A2|1',
       morse.encode('   I AM IN TROUBLE ')
+    )
+  end
+
+  def test_encoding_of_empty_string
+    assert_equal '', morse.encode('')
+  end
+
+  def test_encoding_of_null_input
+    assert_equal '', morse.encode(nil)
+  end
+
+  def test_encoding_of_a_sequence_of_whitespaces
+    assert_equal '', morse.encode('   ')
+  end
+
+  def test_that_encoding_does_not_care_about_letter_case
+    assert_equal(
+      '2/1A|B/2|A1/A|1A1|C|2A|A3|1A2|1',
+      morse.encode('I am in Trouble')
     )
   end
 
@@ -77,6 +128,10 @@ class TestMorseEncoder < Minitest::Test
   private
 
   def morse
-    @morse ||= Morse::Encoder.new
+    @obfuscated_morse ||= Morse::Encoder.new(obfuscated_definitions)
+  end
+
+  def obfuscated_definitions
+    Morse::ObfuscatedDefinitions.new(Morse::DEFINITIONS)
   end
 end
